de6bd69115f92ca8c7553457e4c6a31f49c01add
Tu Dang <tudang87@gmail.com>
2013-06-04 15:31:08 +0200

    refactor according to Btrplace 0.30-SNAPSHOT 20130603-211209-21.jar

---
+++ SpareResource/src/test/java/btrplace/json/model/constraint/PremadeTest.java
@@ -0,0 +1,32 @@
+package btrplace.json.model.constraint;
+
+import btrplace.model.DefaultModel;
+import btrplace.model.Model;
+import btrplace.model.Node;
+import btrplace.model.VM;
+
+/**
+ * User: TU HUYNH DANG
+ * Date: 6/4/13
+ * Time: 1:12 PM
+ */
+class PremadeTest {
+
+    public final Model model = new DefaultModel();
+    public final Node n1 = model.newNode();
+    public final Node n2 = model.newNode();
+    public final Node n3 = model.newNode();
+    public final Node n4 = model.newNode();
+    public final Node n5 = model.newNode();
+
+    public final VM vm1 = model.newVM();
+    public final VM vm2 = model.newVM();
+    public final VM vm3 = model.newVM();
+    public final VM vm4 = model.newVM();
+    public final VM vm5 = model.newVM();
+    public final VM vm6 = model.newVM();
+    public final VM vm7 = model.newVM();
+    public final VM vm8 = model.newVM();
+    public final VM vm9 = model.newVM();
+    public final VM vm10 = model.newVM();
+}
